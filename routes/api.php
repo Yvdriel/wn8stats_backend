cce4f8d2cd8c04cafbd12e1f981120c49c3cf779
yvdriel <yoranvd2001@gmail.com>
2021-04-15 21:20:34 +0200

    - Added Wn8Collection - Added Wn8Resource - Upped Php version to 7.4 - Playing around with api.php

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Wn8Controller;
+use App\Http\Controllers\WoTAPIHandler;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,5 +17,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+    return $request;
 });
+
+Route::middleware('auth:api')->get('/wn8', [WoTAPIHandler::class, 'openIDLogin']);
+//Route::get('/test', [WoTAPIHandler::class, 'openIDLogin']);
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\WoTAPIHandler;
+use App\Http\Resources\Wn8Collection;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,6 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
 Route::get('/openIDLogin', [WoTAPIHandler::class, 'openIDLogin']);
 Route::get('/account', [WoTAPIHandler::class, 'getPlayerPersonalData']);
